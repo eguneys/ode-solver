c03d5e40bb30bf7a772f7d90a30060597ee462e7
Emre Guneyler <eguneys@gmail.com>
2022-10-14 23:43:05 +0300

    mouse

---
+++ src/debug.ts
@@ -0,0 +1,54 @@
+export class Canvas {
+
+  static make = (width: number, height: number, element: HTMLElement) => {
+    let canvas = document.createElement('canvas')
+    canvas.width = width
+    canvas.height = height
+    element.appendChild(canvas)
+    return new Canvas(width, height, canvas)
+  }
+
+  ctx: CanvasRenderingContext2D
+
+  constructor(
+    readonly width: number,
+    readonly height: number,
+    readonly canvas: HTMLCanvasElement) {
+    this.ctx = canvas.getContext('2d')!
+  }
+
+
+  clear() {
+    this.ctx.clearRect(0, 0, this.width, this.height)
+  }
+
+
+  fr(color: string, x: number, y: number, w: number, h: number) {
+    this.ctx.fillStyle = color
+    this.ctx.fillRect(x, y, w, h)
+  }
+}
+
+
+export const loop = (_fn: (dt: number) => void) => {
+
+  let _cancel: number
+
+  let _last_now = 0
+
+  function step(_now: number) {
+
+    let dt = _now - (_last_now || _now)
+    _last_now = _now
+
+    _fn(dt)
+    dt = Math.max(Math.min(dt, 16), 4)
+    _cancel = requestAnimationFrame(step)
+  }
+
+  _cancel = requestAnimationFrame(step)
+
+  return () => {
+    cancelAnimationFrame(_cancel)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,159 +1,72 @@
-import './style.css'
-
-import { ticks } from './shared'
-import { Input } from './input'
+import { Canvas, loop } from './debug'
+import { Vec2, Rectangle } from './vec2'
 import { Vec3, Quat } from './math4'
-import { Vec2, Circle } from './vec2'
 import { XPBD, Particle, Constraint, 
   DistanceConstraint,
   FloorConstraint } from './pbd_course'
+import { make_drag } from './drag'
+import { Ref, onScrollHandlers } from './ref'
 
+let v_screen = Vec2.make(1080, 1920)
+
+const rect_orig = (rect: Rectangle, o: Vec2) => {
+    return rect.x1 <= o.x && o.x <= rect.x2 && rect.y1 <= o.y && o.y <= rect.y2
+}
+
+class Body {
+
+  static make = () => {
+    let a = Particle.make(20, Vec3.make(250, 300, 0), Vec3.zero)
+    let b = Particle.make(20, Vec3.make(450, 300, 0), Vec3.zero)
+    let c = Particle.make(20, Vec3.make(650, 300, 0), Vec3.zero)
+    let d = Particle.make(20, Vec3.make(850, 300, 0), Vec3.zero)
+
+    let g = [[a], [b], [c], [d]]
+    let p = [a, b, c, d]
+
+    return new Body(g, p)
+
+  }
+
+  constructor(readonly groups: Array<Array<Particle>>,
+              readonly particles: Array<Particle>) {}
+
+}
+
+
+let r = 100
 const app = (element: HTMLElement) => {
+  let g = Canvas.make(1080, 1920, element)
 
+  let b = Body.make()
 
-  let i = new Input().init()
+  let ref = Ref.make(element)
+  make_drag({
+    on_drag(e) {
+      if (e.m) {
+        let o = ref.get_normal_at_abs_pos(e.m).mul(v_screen)
+        let p = b.particles.find(_ => {
+          let { x, y } = _.position
+          return rect_orig(Rectangle.make(x - r/2, y - r/2, r, r), o)
+        })
+      }
+    }
+  }, element)
 
-  let g = Canvas.make(1920, 1080, element)
+  onScrollHandlers(() => {
+    ref.$clear_bounds()
+  })
 
-  let player = Particle.make(30, Vec3.make(500, 0, 0), Vec3.zero)
-
-  let particles: Array<Particle> = [
-    Particle.make(20, Vec3.make(500, 0, 0), Vec3.unit),
-    Particle.make(10, Vec3.make(100, 0, 0), Vec3.unit),
-    Particle.make(10, Vec3.make(200, 10, 10), Vec3.unit),
-    Particle.make(10, Vec3.make(200, 10, 10), Vec3.unit),
-    player,
-  ]
-  let constraints: Array<Constraint> = [
-    new DistanceConstraint(player,
-                           particles[2], 
-                           50,
-                           1,
-                           0.5),
-    new DistanceConstraint(particles[2],
-                           particles[3], 
-                           50,
-                           1,
-                           1),
-    new FloorConstraint(particles[3],
-                        1000,
-                        100,
-                        0.3, 1),
-
-    new FloorConstraint(particles[2],
-                        1000,
-                        100,
-                        0.2, 1),
-    new FloorConstraint(player,
-                        1000,
-                        100,
-                        0.4, 1)
-  ]
-
-  let xpbd = new XPBD(6, particles, constraints)
-
-  let li = 0
-  let ri = 0
-  let ci = 0
   loop((dt: number) => {
-    i.update(dt)
-
-    let right = i.been_ons.find(_ => _.includes('ArrowRight'))
-    let left = i.been_ons.find(_ => _.includes('ArrowLeft'))
-    let s = i.been_ons.find(_ => _.includes('s'))
-
-    particles.forEach(_ => {
-      _.force = Vec3.make(0, 10, 0)
-    })
-
-    if (s) {
-      ci = Math.min(100, Math.sin(s[1] / ticks.half * Math.PI * 2) * 60)
-    }
-
-    if (right) {
-      ri = Math.min(30, 10 + ci + (right[1] / ticks.half) * 10)
-    }
-    if (left) {
-      li = Math.min(30, 10 + ci + (left[1] / ticks.half) * 10)
-    }
-
-    if (li > 0) {
-      li--;
-      player.force.add_in(Vec3.left.scale(li))
-    }
-    if (ri > 0) {
-      ri--;
-      player.force.add_in(Vec3.right.scale(ri))
-    }
-
-    if (ci > 0) {
-      ci--;
-      player.force.add_in(Vec3.up.scale(ci))
-    }
-
-    xpbd.update(dt)
-
 
     g.clear()
-    g.fr('hsl(0, 20%, 50%)', 0, 0, 1920, 1080)
-    g.fr('hsl(0, 70%, 40%)', 0, 1000, 1920, 20)
+    g.fr('hsl(0, 20%, 50%)', 0, 0, 1080, 1920)
 
-    particles.forEach(p => {
+    b.particles.forEach(p => {
       let { x, y } = p.position
-      g.fr('hsl(0, 80%, 50%)', x, y, 30, 30)
+      g.fr('hsl(0, 80%, 50%)', x - r/2, y - r/2, r, r)
     })
-
   })
-}
-
-
-
-
-class Canvas {
-
-  static make = (width: number, height: number, element: HTMLElement) => {
-    let canvas = document.createElement('canvas')
-    canvas.width = width
-    canvas.height = height
-    element.appendChild(canvas)
-    return new Canvas(width, height, canvas)
-  }
-
-  ctx: CanvasRenderingContext2D
-
-  constructor(
-    readonly width: number,
-    readonly height: number,
-    readonly canvas: HTMLCanvasElement) {
-    this.ctx = canvas.getContext('2d')!
-  }
-
-
-  clear() {
-    this.ctx.clearRect(0, 0, this.width, this.height)
-  }
-
-
-  fr(color: string, x: number, y: number, w: number, h: number) {
-    this.ctx.fillStyle = color
-    this.ctx.fillRect(x, y, w, h)
-  }
-}
-
-const loop = (on_update: (_: number) => void) => {
-  let _last_now: number = 0
-  function step(_now: number) {
-
-    let _dt = _now - _last_now
-    _dt = Math.max(4, Math.min(16, _dt))
-
-    on_update(_dt)
-
-    _last_now = _now
-
-    requestAnimationFrame(step)
-  }
-  requestAnimationFrame(step)
 }
 
 app(document.getElementById('app')!)
